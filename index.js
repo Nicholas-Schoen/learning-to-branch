ec87ce71d79675ffa040df5e21f1c3c5d9de5ebe
Nicholas Schoen <scheontransit@gmail.com>
2024-03-12 18:44:00 -0500

    added Month Variable

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const button = document.getElementById("button");
 const currentDate = new Date();
+
+//gets Day of the week name
 const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const currentDayIndex = currentDate.getDay();
 const currentDay = daysOfTheWeek[currentDayIndex];
 
+//gets Name of Month 
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const currentMonthIndex = currentDate.getMonth();
+const currentMonth =monthNames[currentMonthIndex];
 
-console.log(` Today is ${currentDay}`);
+console.log(` Today is ${currentDay}, in the month of ${currentMonth}.`);
 
 
 button.addEventListener("click", () => {
