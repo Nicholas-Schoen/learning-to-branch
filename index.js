8870c915a3615d3ea30f0841fae3af640497acb0
Nicholas Schoen <scheontransit@gmail.com>
2024-03-12 18:34:06 -0500

    added variables to allow display of days of week

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const button = document.getElementById("button")
+const button = document.getElementById("button");
 const currentDate = new Date();
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const currentDayIndex = currentDate.getDay();
+const currentDay = daysOfTheWeek[currentDayIndex];
 
-console.log(`The current date is ${currentDate}`);
+
+console.log(` Today is ${currentDay}`);
+
 
 button.addEventListener("click", () => {
     alert(`The current date is ${currentDate}`)
