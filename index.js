288aaf67ea6590778b128f10ffbe2e8bddeab18f
Nicholas Schoen <scheontransit@gmail.com>
2024-03-13 13:38:42 -0500

    finished feature of button with cleaning up of the javascript.

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 const button = document.getElementById("button");
 const currentDate = new Date();
 const dayOfTheMonth = currentDate.getDate();
-
-//gets Day of the week name
 const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const currentDayIndex = currentDate.getDay();
 const currentDay = daysOfTheWeek[currentDayIndex];
-
-//gets Name of Month 
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const currentMonthIndex = currentDate.getMonth();
 const currentMonth =monthNames[currentMonthIndex];
-
-//gets year
 const currentYear = currentDate.getFullYear();
-
-//pop-up element variables
 const paragraphElement = document.createElement('p');
+
+
 paragraphElement.setAttribute("id", "new-paragraph-element")
 
 
-console.log(` Today is ${currentDay}, in the month of ${currentMonth}. The Year is ${currentYear} AD.`);
-
 function showDate() {
     button.insertAdjacentElement("afterend", paragraphElement);
-    paragraphElement.textContent = `Today is ${currentDay}, ${currentMonth} ${dayOfTheMonth}, ${currentYear}.`
+    paragraphElement.textContent = 
+        `Today is ${currentDay}, ${currentMonth} ${dayOfTheMonth}, ${currentYear}.`;
+    button.innerText = "Hide me!";
 };
 
-button.addEventListener("click", showDate);
+function hideDate() {
+    paragraphElement.remove();
+    button.innerText = "Click me!"
+};
 
-/*
-button.addEventListener("click", () => {
-    alert(`The current date is ${currentDate}`)
-});
-*/
+function checkDate() {
+    if (button.innerText === "Click me!") {
+        showDate();
+        return;
+    } else {
+        hideDate();
+        return;
+    };
+};
+
+button.addEventListener("click", checkDate);
